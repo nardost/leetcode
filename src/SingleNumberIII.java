f491104d8c2494daa9d829f4e0b7a87c48a05bf4
nardos <32473919+nardost@users.noreply.github.com>
2020-06-19 22:28:24 -0500

    SingleNumberIII accepted...

--- src/SingleNumberIII.java
+++ src/SingleNumberIII.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -5,21 +6,20 @@ public class SingleNumberIII {
 
     public static void main(String[] args) {
         final int[] nums = new int[] { 2, 1, 3, 2, 5, 3, 5, 7 };
-        final Integer[] result = new SingleNumberIII().singleNumber(nums);
+        final int[] result = new SingleNumberIII().singleNumber(nums);
         display(result);
     }
-    public Integer[] singleNumber(int[] nums) {
+    public int[] singleNumber(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
             if(!set.add(nums[i])) {
                 set.remove(nums[i]);
             }
         }
-        Integer[] intArray = new Integer[] {};
-        return set.toArray(intArray);
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private static void display(final Integer[] nums) {
+    private static void display(final int[] nums) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for(int i = 0; i < nums.length; i++) {
